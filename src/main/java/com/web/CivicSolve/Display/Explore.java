88dae6865808fdbc0dcc77f317393f5e2f347843
rooteek <manakmalmills@gmail.com>
2026-02-28 23:48:54 +0530

    add:explore ui desing pages

---
+++ src/main/java/com/web/CivicSolve/Display/Explore.java
@@ -0,0 +1,15 @@
+package com.web.CivicSolve.Display;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Explore {
+    @GetMapping("/explore")
+    public ModelAndView DisplayExplore(){
+        ModelAndView mv = new ModelAndView("expore-page");
+        
+        return mv;
+    }
+}
